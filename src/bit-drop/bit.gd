a3fb3e22bba72e5a680c71603043e63e56818be8
David Miles <quantumdave@Gmail.com>
2026-01-31 13:22:32 +0000

    Add falling bits

--- src/bit-drop/bit.gd
+++ src/bit-drop/bit.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 var isFalling = true
 
+var isOne = false
+
+@onready var label = $Label
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +13,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if isOne:
+		label.text = "1"
+	else:
+		label.text = "0"
+	
 	pass
--- src/bit-drop/main.gd
+++ src/bit-drop/main.gd
@@ -1,16 +1,49 @@
-extends Node2D
+extends Node
 
-var fallingBits = $FallingBits
+@export var bit_scene: PackedScene
+
+var size = Vector2(10, 20)
+
+var bit_width = 50
+
+@onready var mask = $Mask
+@onready var bits = $Bits
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for i in range(0, size.x):
+		var bit = bit_scene.instantiate()
+		bit.position.x = i * bit_width
+		bit.isOne = i % 2 == 0
+		
+		mask.add_child(bit)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("move_left"):
+		pass
+	elif Input.is_action_just_pressed("move_right"):
+		pass
+		
 	pass
 
 
 func _on_tick_timer_timeout() -> void:
-	for 
+	move_bits()
+	
+	add_bit()
+	
+func move_bits():
+	for bit in bits.get_children():
+		bit.position.y += bit_width
+
+
+	
+func add_bit():
+	var bit = bit_scene.instantiate()
+	bit.isOne = randf() > 0.5
+	
+	bit.position.x = randi_range(0, size.x - 1) * bit_width
+	
+	bits.add_child(bit)
